ffeb2117e30e205c14f8d8993e7c2c9634daf560
Mokhtar Miloua <miloua23@gmail.com>
2026-02-16 16:21:07 +0100

    fix: trying to fix deployment

--- pharmacie/pharmacie/wsgi.py
+++ pharmacie/pharmacie/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharmacie.pharmacie.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharmacie.settings')
 
 application = get_wsgi_application()
